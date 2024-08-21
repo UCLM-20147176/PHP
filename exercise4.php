c30a0ed9735a884263859882f9ddf9b66cdc0db6
YouBet <113317377+UCLM-20147176@users.noreply.github.com>
2024-08-21 10:15:46 +0800

    Update exercise4.php

--- exercise4.php
+++ exercise4.php
@@ -6,23 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="">
+    <form action="" method = "post">
         Starting Number
-        <input type="text"> <br /> <br />
+        <input type="text" name = "start"> <br /> <br />
 
         Ending Number
-        <input type="text"> <br /> <br />
+        <input type="text" name = "end"> <br /> <br />
 
         <button>Display</button> <br /> <br /><br /> <br />
 
         <?php
             
-           
+           $start = $_POST["start"];
+           $end = $_POST["end"];
+            
 
 
 
-        echo "Starting Number: <br><br>";
-        echo "Finding Number:  <br><br>";
+        echo "Starting Number: $start<br><br>";
+        echo "Finding Number:  $end<br><br>";
         echo "Odd Numbers: <br><br>";
         echo "Even Numbers: <br><br>";
         echo "Divisible by 3 Numbers: <br><br>";
